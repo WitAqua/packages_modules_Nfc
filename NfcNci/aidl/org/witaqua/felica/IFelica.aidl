2by2-Project/android_packages_apps_Nfc

Nfc: Add reversed native felica SE implementation
* Required for felica on legacy devices, this API used by FelicaService.

Change-Id: I71c53689533ebcdfdd3be84825d301656ce3caa2

aidl/jp/project2by2/felica/IFelica.aidl
@@ -1,0 +1,31 @@
+//
+// Copyright (C) 2026 The 2by2 Project
+// SPDX-License-Identifier: Apache-2.0
+//
+
+package jp.project2by2.felica;
+
+import android.os.Bundle;
+
+interface IFelica {
+    Bundle openSe(String packageName, IBinder token);
+    int closeSe(String packageName, int handle, IBinder token);
+    Bundle transceiveSe(String packageName, int handle, in byte[] data, int timeoutMs);
+    int cancelSe(String packageName, int handle);
+    int connectSe(String packageName, int handle);
+    int disconnectSe(String packageName, int handle);
+
+    Bundle openRf(String packageName, IBinder token);
+    int closeRf(String packageName, int handle, IBinder token);
+    Bundle transceiveRf(String packageName, int handle, in byte[] data, int timeoutMs);
+    int cancelRf(String packageName, int handle);
+    int connectRf(String packageName, int handle, int timeoutMs);
+    int disconnectRf(String packageName, int handle);
+
+    boolean enable();
+    boolean disable(boolean persist);
+    int getState();
+    int getRwP2pState();
+    boolean setRwP2pMode(boolean enabled);
+    void prepareSwitchedOffState();
+}

src/com/android/nfc/NfcApplication.java
@@ -25,6 +25,9 @@ import android.os.Looper;
 import android.os.Process;
 import android.os.UserHandle;
 
+// 2by2 Additions for felica
+import jp.project2by2.felica.FelicaBackendService;
+
 import java.util.Iterator;
 import java.util.List;
 
@@ -63,6 +66,9 @@ public class NfcApplication extends Application {
         }
         if (UserHandle.myUserId() == 0 && isMainProcess) {
             mNfcService = new NfcService(this, new NfcInjector(this, Looper.myLooper()));
+
+            // 2by2 Additions for felica
+            FelicaBackendService.register();
         }
     }
 }

src/jp/project2by2/felica/NativeFelicaSe.java
@@ -1,0 +1,33 @@
+//
+// Copyright (C) 2026 The 2by2 Project
+// SPDX-License-Identifier: Apache-2.0
+//
+
+package jp.project2by2.felica;
+
+/** Reverse engineered native felica SE endpoint */
+public final class NativeFelicaSe {
+    private native void doCancel(int handle);
+
+    private native int doClose(int handle);
+
+    private native int doOpen();
+
+    private native byte[] doTransceive(int handle, byte[] command, int timeoutMs, int[] error);
+
+    public void cancel(int handle) {
+        doCancel(handle);
+    }
+
+    public int close(int handle) {
+        return doClose(handle);
+    }
+
+    public int open() {
+        return doOpen();
+    }
+
+    public byte[] transceive(int handle, byte[] command, int timeoutMs, int[] error) {
+        return doTransceive(handle, command, timeoutMs, error);
+    }
+}

WitAqua/packages_modules_Nfc

Signed-off-by: Toufu <hamm157famm7423@gmail.com>

NfcNci/aidl/org/witaqua/felica/IFelica.aidl
@@ -1,0 +1,31 @@
+//
+// Copyright (C) 2026 The 2by2 Project
+// SPDX-License-Identifier: Apache-2.0
+//
+
+package org.witaqua.felica;
+
+import android.os.Bundle;
+
+interface IFelica {
+    Bundle openSe(String packageName, IBinder token);
+    int closeSe(String packageName, int handle, IBinder token);
+    Bundle transceiveSe(String packageName, int handle, in byte[] data, int timeoutMs);
+    int cancelSe(String packageName, int handle);
+    int connectSe(String packageName, int handle);
+    int disconnectSe(String packageName, int handle);
+
+    Bundle openRf(String packageName, IBinder token);
+    int closeRf(String packageName, int handle, IBinder token);
+    Bundle transceiveRf(String packageName, int handle, in byte[] data, int timeoutMs);
+    int cancelRf(String packageName, int handle);
+    int connectRf(String packageName, int handle, int timeoutMs);
+    int disconnectRf(String packageName, int handle);
+
+    boolean enable();
+    boolean disable(boolean persist);
+    int getState();
+    int getRwP2pState();
+    boolean setRwP2pMode(boolean enabled);
+    void prepareSwitchedOffState();
+}

NfcNci/src/com/android/nfc/NfcApplication.java
@@ -25,6 +25,9 @@ import android.os.Looper;
 import android.os.Process;
 import android.os.UserHandle;
 
+// 2by2 Additions for felica
+import org.witaqua.felica.FelicaBackendService;
+
 import java.util.Iterator;
 import java.util.List;
 
@@ -63,6 +66,9 @@ public class NfcApplication extends Application {
         }
         if (UserHandle.myUserId() == 0 && isMainProcess) {
             mNfcService = new NfcService(this, new NfcInjector(this, Looper.myLooper()));
+
+            // 2by2 Additions for felica
+            FelicaBackendService.register();
         }
     }
 }

NfcNci/src/org/witaqua/felica/NativeFelicaSe.java
@@ -1,0 +1,33 @@
+//
+// Copyright (C) 2026 The 2by2 Project
+// SPDX-License-Identifier: Apache-2.0
+//
+
+package org.witaqua.felica;
+
+/** Reverse engineered native felica SE endpoint */
+public final class NativeFelicaSe {
+    private native void doCancel(int handle);
+
+    private native int doClose(int handle);
+
+    private native int doOpen();
+
+    private native byte[] doTransceive(int handle, byte[] command, int timeoutMs, int[] error);
+
+    public void cancel(int handle) {
+        doCancel(handle);
+    }
+
+    public int close(int handle) {
+        return doClose(handle);
+    }
+
+    public int open() {
+        return doOpen();
+    }
+
+    public byte[] transceive(int handle, byte[] command, int timeoutMs, int[] error) {
+        return doTransceive(handle, command, timeoutMs, error);
+    }
+}